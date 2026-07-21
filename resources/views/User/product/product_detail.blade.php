9d77b459dc5666a9fe13bc13bf25cd7c070e8772
developeritexpert <developeritexpert3@gmail.com>
2026-07-21 16:57:14 +0530

    added category page

--- resources/views/User/product/product_detail.blade.php
+++ resources/views/User/product/product_detail.blade.php
@@ -364,7 +364,7 @@
 }
 
 .asan-slider.asan-slider-btm.slider-nav .slick-track .slick-slide.slick-active {
-    margin: 10px 5px 0 !important;
+    margin: 0 5px  !important;
 }
 .new-visit-anc .cta.cta_orange {
     font-size: 13px;
@@ -1137,7 +1137,7 @@
 
                                                         <!-- Thumbnail Slider -->
                                                         <div class="col-md-12 asan-slider asan-slider-btm slider-nav"
-                                                            style="margin-top: 15px;">
+                                                            style="margin-top: 15px !important;">
                                                             @foreach ($images as $index => $image)
                                                                 <div style="padding: 0 5px; cursor: pointer;">
                                                                     <img src="{{ asset($image) }}"
@@ -1150,6 +1150,24 @@
                                                     </div>
                                                 </div>
                                          @endif
+
+                                         <script>
+                                            document.addEventListener('DOMContentLoaded', function() {
+                                                // Wait for slick to initialize
+                                                setTimeout(() => {
+                                                    var $slider = $('.asan-slider-btm');
+                                                    
+                                                    // Give the active slide the class initially
+                                                    $slider.find('.slick-current').addClass('hover_main');
+                                                    
+                                                    // On hover, add class and remove from others
+                                                    $slider.on('mouseenter', '.slick-slide', function() {
+                                                        $slider.find('.slick-slide').removeClass('hover_main');
+                                                        $(this).addClass('hover_main');
+                                                    });
+                                                }, 500);
+                                            });
+                                         </script>
                                          {{-- End Business Images  --}}
 
                                          @if (!empty($business->translations->first()->after_image_description))
