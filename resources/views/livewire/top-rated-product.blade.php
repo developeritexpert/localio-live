04dca3710bbe76eee3d113ee4445073ef8fbdf54
developeritexpert <developeritexpert3@gmail.com>
2026-08-20 18:34:34 +0530

    update code

--- resources/views/livewire/top-rated-product.blade.php
+++ resources/views/livewire/top-rated-product.blade.php
@@ -740,20 +740,7 @@
                                         0;
                                         @endphp
 
-                                        @if($totalRevCount > 0 && !empty($item->is_affiliate))
-                                        <div class="d-flex align-items-center recommended-per"
-                                            style="color: #002347; font-size: 13px; font-weight: 600;">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                viewBox="0 0 24 24" fill="none" stroke="#002347" stroke-width="2"
-                                                stroke-linecap="round" stroke-linejoin="round"
-                                                style="margin-right: 5px; flex-shrink: 0;">
-                                                <path
-                                                    d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3">
-                                                </path>
-                                            </svg>
-                                            <span>{{ $recPercent }}% recommend this</span>
-                                        </div>
-                                        @endif
+                                        
                                         @if($isRecommended)
                                         <div class="best-value-inline-container" style="padding-bottom:20px">
                                             <div class="best-value-inline">
@@ -761,7 +748,22 @@
                                                 <i class="far fa-star text-warning"></i>
                                                 <span style="text-transform: none !important;">Top choice</span>
                                             </div>
-
+                                            <!-- start recomment this section here -->
+                                            @if($totalRevCount > 0 && !empty($item->is_affiliate))
+                                                <div class="d-flex align-items-center recommended-per"
+                                                    style="color: #002347; font-size: 13px; font-weight: 600;">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                        viewBox="0 0 24 24" fill="none" stroke="#002347" stroke-width="2"
+                                                        stroke-linecap="round" stroke-linejoin="round"
+                                                        style="margin-right: 5px; flex-shrink: 0;">
+                                                        <path
+                                                            d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3">
+                                                        </path>
+                                                    </svg>
+                                                    <span>{{ $recPercent }}% recommend this</span>
+                                                </div>
+                                            @endif
+                                            <!-- end recomment this section here -->
                                         </div>
                                         @endif
 
